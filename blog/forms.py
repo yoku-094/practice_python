06340ede3ef535ce9225fba764c86e8183cedf8e
yoku-094 <yoku.20201001@gmail.com>
2025-05-10 16:16:57 +0900

    レイアウト修正他

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'custom-textarea',  # ← ここでクラス指定
+            }),
+        }
