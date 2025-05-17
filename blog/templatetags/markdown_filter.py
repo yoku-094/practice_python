e3e4e9e7d91f3562940cf7ea4243ff44b7ffc172
yoku-094 <yoku.20201001@gmail.com>
2025-05-17 16:23:45 +0900

    マークダウンのプレーンテキスト変換実装

--- blog/templatetags/markdown_filter.py
+++ blog/templatetags/markdown_filter.py
@@ -1,9 +1,17 @@
 import markdown
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.html import strip_tags
 
 register = template.Library()
 
+# HTML変換用
 @register.filter
 def markdown_to_html(text):
     return mark_safe(markdown.markdown(text))
+
+# プレーンテキスト変換用
+@register.filter
+def markdown_to_plain(text):
+    html = markdown.markdown(text)
+    return strip_tags(html)
